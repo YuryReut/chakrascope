11a330d1c74809ad04d0e1d2c21250f81d6105fc
YuryReut <yury.reut@gmail.com>
2025-03-11 20:52:01 +0800

    Update App.tsx

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -38,6 +38,13 @@ function App() {
 
     return (
         <div style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+            width: "100vw",
+            textAlign: "center",
             fontFamily: "inherit",
             padding: "20px",
             boxSizing: "border-box"
