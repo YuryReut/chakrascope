3ae237a339616a0565bc93798959895a39edf616
YuryReut <yury.reut@gmail.com>
2025-03-20 17:06:50 +0800

    Update birthChakra.ts

--- src/api/birthChakra.ts
+++ src/api/birthChakra.ts
@@ -66,27 +66,27 @@ export function getBirthChakra(dateOfBirth: string, currentDate: string, sunDegr
 
 return {
     result: {
-        birth: `🔆Твой главный ресурс — ${chakraSun.emoji} ${solarChakra}-й чакры ${chakraSun.title} (${chakraSun.name}).
-        🌀 Внутреннее ощущение: ${chakraSun.phases[0].inner}  
-        🌍 Как это проявляется в жизни: ${chakraSun.phases[0].outer}  
-        ❤️ В любви и отношениях:
-        <a 
-                href=${chakraSun.link} 
-                target="_blank" 
-                rel="noopener noreferrer" 
-                style={{ color: "inherit", textDecoration: "none" }}
-            >
-                ${chakraSun.phases[0].relationship}
-            </a>
-        🌙 Тонкое восприятие мира — через ${chakraMoon.desc} благодаря ${chakraMoon.emoji} ${lunarChakra}-й Чакре ${chakraMoon.title} (${chakraMoon.name}).
-        `,
-        currentPath: `💫 Главные энергии года:  ${chakrasData.chakras[yearChakra - 1].desc} из ${chakrasData.chakras[yearChakra - 1].emoji} ${yearChakra}-й Чакры ${chakrasData.chakras[yearChakra - 1].title} (${chakrasData.chakras[yearChakra - 1].name}).
-        🔄 А прямо сейчас в 52-дневном цикле ты живешь через ${chakrasData.chakras[cycleChakra - 1].desc} через ${chakrasData.chakras[cycleChakra - 1].emoji} ${cycleChakra}-ю Чакру ${chakrasData.chakras[cycleChakra - 1].title} (${chakrasData.chakras[cycleChakra - 1].name}).  
-        `,
-        today: `🔥 Ты сегодня — это ${chakrasData.chakras[dayChakra - 1].desc} из ${chakrasData.chakras[dayChakra - 1].emoji} ${dayChakra}-й Чакры ${chakrasData.chakras[dayChakra - 1].title} (${chakrasData.chakras[dayChakra - 1].name}).
-        💫 Эмоции дня — ${chakrasData.chakras[lunarChakra - 1].desc}, потому что энергия в ${chakrasData.chakras[lunarChakra - 1].emoji} ${lunarChakra}-ой Чакре ${chakrasData.chakras[lunarChakra - 1].title} (${chakrasData.chakras[lunarChakra - 1].name}).
-        `
-    },
+    birth: `🔆Твой главный ресурс — ${chakraSun.emoji} ${solarChakra}-й чакры ${chakraSun.title} (${chakraSun.name}).<br />
+    🌀 Внутреннее ощущение: ${chakraSun.phases[0].inner} <br />
+    🌍 Как это проявляется в жизни: ${chakraSun.phases[0].outer} <br />
+    ❤️ В любви и отношениях: 
+    <a 
+        href="${chakraSun.link}" 
+        target="_blank" 
+        rel="noopener noreferrer" 
+        style="color: inherit; text-decoration: none;"
+    >
+        ${chakraSun.phases[0].relationship}
+    </a><br />
+    🌙 Тонкое восприятие мира — через ${chakraMoon.desc} благодаря ${chakraMoon.emoji} ${lunarChakra}-й Чакре ${chakraMoon.title} (${chakraMoon.name}).
+    `,
+
+    currentPath: `💫 Главные энергии года: ${chakrasData.chakras[yearChakra - 1].desc} из ${chakrasData.chakras[yearChakra - 1].emoji} ${yearChakra}-й Чакры ${chakrasData.chakras[yearChakra - 1].title} (${chakrasData.chakras[yearChakra - 1].name}).<br />
+    🔄 А прямо сейчас в 52-дневном цикле ты живешь через ${chakrasData.chakras[cycleChakra - 1].desc} через ${chakrasData.chakras[cycleChakra - 1].emoji} ${cycleChakra}-ю Чакру ${chakrasData.chakras[cycleChakra - 1].title} (${chakrasData.chakras[cycleChakra - 1].name}).`,
+
+    today: `🔥 Ты сегодня — это ${chakrasData.chakras[dayChakra - 1].desc} из ${chakrasData.chakras[dayChakra - 1].emoji} ${dayChakra}-й Чакры ${chakrasData.chakras[dayChakra - 1].title} (${chakrasData.chakras[dayChakra - 1].name}).<br />
+    💫 Эмоции дня — ${chakrasData.chakras[lunarChakra - 1].desc}, потому что энергия в ${chakrasData.chakras[lunarChakra - 1].emoji} ${lunarChakra}-ой Чакре ${chakrasData.chakras[lunarChakra - 1].title} (${chakrasData.chakras[lunarChakra - 1].name}).`
+}
     logs: debugLogs
 };}
 
