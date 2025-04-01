06fc27b191afd7d9f13280a365e75d045e7bcfe3
Chakroscope <yury.reut@gmail.com>
2025-04-01 13:02:15 +0800

    Update App.tsx

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -303,8 +303,8 @@ const handleCheckChakra = () => {
   };
 
   const chakraNumberSun = getPersonalChakraDay(sunDegree);
+  import { nakshatraToChakra } from "../api/birthChakra";
   const moonNakshatraIndex = Math.floor(moonDegree / (360 / 27));
-  const nakshatraToChakra = [1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 1, 1, 1, 2, 2, 2];
   const chakraNumberMoon = nakshatraToChakra[moonNakshatraIndex] || 1;
 
   setChakraNameSun(chakraNameMap[chakraNumberSun as keyof typeof chakraNameMap] as ChakraName);
