7c6a4b7319887eae8d5ca869f9c2d3c6a3846b47
Chakroscope <yury.reut@gmail.com>
2025-04-05 16:23:08 +0800

    Update App.tsx

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -10,15 +10,6 @@ import lunarData from "../api/lunar.json";
 import day_EQ7 from "../api/dayEQ7_data.json";
 import chakraCompatibilityRaw from "../api/chakras_compatibility.json";
 
-const nakshatraPostIds = [
-  "DH7_GNDxmc2", "DH7-yo0RL32", "DH7-39BxDVm", "DH7_ZuRx-9U", "DH7_-YwRUTD",
-  "DH7-6cvR4c_", "DH7_MdwxBMq", "DH7_ykXRAR6", "DH7_2DyRsqk", "DH7_JdyRDD3",
-  "DH7-0xOxTA2", "DH7_dlzRFCF", "DH7_DBqxoMy", "DH7-9mnx66d", "DH8AAxIx1Ku",
-  "DH8AYlHxXRm", "DH7_fdexsrV", "DH7_PL6R-Ns", "DH7_5rSRthF", "DH7_rGjRlmp",
-  "DH7_kVgRr5P", "DH7_vr4xCOp", "DH7_tF9xo9d", "DH7_oPBxOGh", "DH78ngkR04m",
-  "DH7_UlDx8b4", "DH7-_ykxKmK"
-];
-
 type CompatibilityDetails = {
   how: string;
   not: string;
