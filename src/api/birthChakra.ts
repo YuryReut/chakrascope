127c192a7af1b10e1d60109b1968aaa82c74028c
Chakroscope <yury.reut@gmail.com>
2025-04-05 21:32:20 +0800

    Update birthChakra.ts

--- src/api/birthChakra.ts
+++ src/api/birthChakra.ts
@@ -133,7 +133,7 @@ export function getBirthChakra(dateOfBirth: string, sunDegree: number, moonDegre
     },
     currentPath: chakrasData.chakras[yearChakra - 1].path,
     today: `${chakrasData.chakras[dayChakra - 1].name} и ${chakraMoon.name}`,
-    todayText: chakraMoon.sprint,
+    todayText: chakraMoon.day,
     chakraPeriodLink: `https://www.instagram.com/p/${chakraPeriodPosts[dayChakra as keyof typeof chakraPeriodPosts]}/`,
     chakraDayLink: `https://www.instagram.com/p/${chakraDayPosts[lunarChakraNumber as keyof typeof chakraDayPosts]}/`,
     todayNakshatraName: nakshatraNames[moonNakshatraIndex],
